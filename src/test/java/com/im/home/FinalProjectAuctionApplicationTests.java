050673331e343acda95febd6b2f908af099f0553
rootgit23 <rmsgh1009@naver.com>
2022-11-10 17:09:54 +0900

    update

---
+++ src/test/java/com/im/home/FinalProjectAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.im.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
