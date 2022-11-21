b11b4ecbce6639c445c281ed64b49a4c6f875ee8
ezzzyone <jiwon1799@gmail.com>
2022-11-21 22:01:16 +0900

    간결하게어케하나?

--- src/main/java/com/im/home/wholesale/WholeSaleController.java
+++ src/main/java/com/im/home/wholesale/WholeSaleController.java
@@ -1,10 +1,19 @@
 package com.im.home.wholesale;
 
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,7 +25,7 @@ public class WholeSaleController {
 	
 	@GetMapping("test")
 	@ResponseBody
-	public String wholeSale() {
+	public String wholeSale() throws Exception{
 		
 		WebClient webClient = WebClient.builder()
 			    .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(-1)) // to unlimited memory size
@@ -34,7 +43,56 @@ public class WholeSaleController {
 //			log.info("WhsalName => {}", s);
 //		});
 		log.info("r => {}", r);
+	
+//		ObjectMapper objectMapper = new ObjectMapper();
+//		String data = objectMapper.writeValueAsString(r);
+	
+	ObjectMapper objectMapper = new ObjectMapper();
+	JSONParser parser = new JSONParser();
+	Map<String, Object> data = objectMapper.readValue(r, new TypeReference<Map<String, Object>>() {});
+	
+		JSONObject jobj = new JSONObject(data);
+
+		Object jobj2 = jobj.get("data");
+		String data2 = objectMapper.writeValueAsString(jobj2);
+		JSONArray temp = (JSONArray)parser.parse(data2);
+
+		for(int i =0; i<temp.size(); i++) {
+			JSONObject jsonObj = (JSONObject)temp.get(i);
+			log.info("array => {}", jsonObj);
+			log.info("largeName => {}", jsonObj.get("largeName"));
+			
+		}
+		
+		
+//		
+//		JSONParser parser = new JSONParser();
+//			
+//		JSONObject jsonObject = parser.parse(r);
+//		log.info("plssssss => {}", jsonObject);
+	
+//		log.info("objjj => {}", obj);
+//		Object datajson = obj.get("data");
+//		log.info("objjj222 => {}", datajson);
+//		
+//		JSONArray temp = (JSONArray)datajson;
+//		for(int i =0; i<temp.size(); i++) {
+//			
+//			JSONObject jsonObj = (JSONObject)temp.get(i);
+//			log.info("plssssss => {}", jsonObj);
+//			
+//		}
+			
+		
+//		log.info("daaaaaaaaaaata => {}", obj.get("data"));
+
+		
+		//WholeSaleVO wholeSaleVO  = objectMapper.readValue(data, WholeSaleVO.class);
+
+		//log.info("VO======= => {}", wholeSaleVO);
+	
 		return "wholesale/test";
+		
 		
 		//String을json으로 변경 
 		//jackson
