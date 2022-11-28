b5ea37c8afdcbb53794e3f11b1b03eef581a6bc9
20170856MS <fpqk1004@sungkyul.ac.kr>
2022-11-28 15:48:00 +0900

    1128 deliveryTest
    map, latitude, longitude

---
+++ src/main/java/com/im/home/map/MapService.java
@@ -0,0 +1,10 @@
+package com.im.home.map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MapService {
+
+		
+}
