27957d31f7492f48823044ae64fbea11ad39de48
ezzzyone <jiwon1799@gmail.com>
2022-12-03 12:30:24 +0900

    paging

--- src/main/resources/static/js/wholesale/sale2.js
+++ src/main/resources/static/js/wholesale/sale2.js
@@ -4,8 +4,10 @@ let apiURL = 'https://at.agromarket.kr/openApi/price/sale.do';
 //페이징 js에서 하는 이유 - 데이터를 js에서 가공하고 페이지이동없이 ajax로 처리하기 위함 
 //json 호출하기 
 //https://cors-anywhere.herokuapp.com/ 크로스 에러 떠서 프록시서버 설정 추가
-function getJSON() {
+
+function getJSON(i) {
     let pageNo = 1;
+    let perPage = 10;
     let whsalCd = $("#whsalCd").val();
     let saleDate = $("#saleDate").val();
     let largeCd = $("#largeCd").val();
@@ -30,26 +32,60 @@ function getJSON() {
         crossOrigin: true,
         dataType:"json",
         success: function(jsonData){
+            let totPage = parseInt(jsonData.totCnt/10)+1 //총페이지수
+            let setSmallPage = jsonData.dataCnt/10 //페이지 당 뷰 수. 리스트 10개씩 뽑아올때 1Page당 몇 뷰가 생기는지. 
+            if(jsonData.dataCnt%10>0){ //나머지값이 있다면 뷰 추가
+                setSmallPage++
+            }
+            if(i<1){
+                i =1;
+            }
+            let startRow =(i-1)*perPage;;
+            let lastRow = i*perPage;
+            let pagings = '';
+            for(let i=1; i<setSmallPage; i++){
+                pagings += '<button type="button" onclick="getJSON('+i+')">'+i+'</button>';
+        
+            }
+
+           
             console.log("통신성공");
             console.log(jsonData);
-
-            console.log("cnt=="+jsonData.totCnt);
-            console.log("url=="+url);
-            //몫을 정수로 변환
-            let totPage = parseInt(jsonData.totCnt/10)+1
+            console.log("cnt=="+jsonData.totCnt);//총데이터건수
+            console.log("cnt=="+jsonData.dataCnt);//페이지데이터건수
+            console.log("url=="+url);//요청 url
             console.log(totPage);
+            console.log("setSmallpage ="+setSmallPage);
+            console.log("startRow ="+startRow);
+            console.log("lastRow ="+lastRow);
+            console.log("jsonData.data[2].saleDate"+jsonData.data[2].saleDate);
+
             $('.table_body').empty();
-          //  $('.plsPage').empty();
+            $('.plsPage').empty();
+            $('.paging').empty();
+                
+
             str = '<TR>'; 
-                $.each(jsonData.data , function(i){
-                    str += '<TD>' + jsonData.data[i].saleDate + '</TD>'+
-                           '<TD>' + jsonData.data[i].whsalName + '</TD>'+
-                            '<TD>' + jsonData.data[i].cmpName + '</TD>'+
-                           '<TD>' + jsonData.data[i].smallName + '</TD>';
-                    str += '</TR>';
-                });
+            for(let j=startRow; j<lastRow+1; j++){
+             str += '<TD>' + jsonData.data[j].saleDate + '</TD>'+
+                '<TD>' + jsonData.data[j].whsalName + '</TD>'+
+                '<TD>' + jsonData.data[j].cmpName + '</TD>'+
+                '<TD>' + jsonData.data[j].smallName + '</TD>';
+             str += '</TR>';
+             }
+
+                // $.each(jsonData.data , function(i){
+                //     str += '<TD>' + jsonData.data[i].saleDate + '</TD>'+
+                //            '<TD>' + jsonData.data[i].whsalName + '</TD>'+
+                //             '<TD>' + jsonData.data[i].cmpName + '</TD>'+
+                //            '<TD>' + jsonData.data[i].smallName + '</TD>';
+                //     str += '</TR>';
+                // });
+
+
             $('.table_body').append(str);
-          //  $('.plsPage').append('<h1>'+totPage+'</h1>');
+            $('.plsPage').append('<h3>총페이지수:'+totPage+'</h3>');
+            $('.paging').append(pagings);
            
         },
         error:function(){
@@ -57,5 +93,7 @@ function getJSON() {
         }
     })
 }
+
+
 
 
--- src/main/webapp/WEB-INF/views/wholesale/sale2.jsp
+++ src/main/webapp/WEB-INF/views/wholesale/sale2.jsp
@@ -15,32 +15,39 @@
 <c:import url="../common/header.jsp"></c:import>
 <c:import url="../temp/boot.jsp"></c:import>
 <!-- ================================ -->
+<script>
+    $(document).ready(function(){
+        getJSON(1)
+    })    
+</script>
+<!--총페이지 수-->
+<div class="plsPage"></div>
+<div class="paging"></div>
+<!--총데이터 수-->
 
-<h1>${param.plsPage}</h1>
     <table class="table table-striped" id="api">
         <thead>
             <tr>
                 <td>whsalCd : 
                     <select name="whsalCd" id="whsalCd">
-                        <option value="110001">어떤거1</option>
-                        <option value="330201">어떤거2</option>
+                        <option value="110001">가락시장</option>
+                        <option value="330201">다른시장</option>
                     </select> 
                 </td>
                 <td>saleDate : 
                     <select name="saleDate" id="saleDate">
-                        <option value="20221122">어떤거1</option>
-                        <option value="20221129">어떤거2</option>
+                        <option value="20221122">22일</option>
+                        <option value="20221129">29일</option>
                     </select> 
                 </td>
                 <td>대분류 : 
                     <select name="largeCd" id="largeCd">
-                        <option value="05">어떤거1</option>
-                        <option value="06">어떤거2</option>
+                        <option value="05">05</option>
+                        <option value="06">06</option>
                     </select> 
                 </td>
                 <td>
-                   
-                    <input type="button" value="검색" @click="search()">
+                    <button type="button" onclick="getJSON(1)">검색</button>
                 </td>
             </tr>
             <tr>
@@ -56,11 +63,35 @@
             </tbody>
 
         </div>
-        <button type="button" onclick="getJSON()"> json 불러오는 버튼</button>
+        
        </table>
-            <div class="plsPage">
 
-            </div>
+       <!-- pagination start -->
+    <div class="chefs section-bg" style="padding-bottom: 10px;">
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+          <!-- 이전페이지가 없으면 	버튼 비활성화  -->
+            <li class="page-item ${pager.pre?'':'disabled'}">
+            <!-- page 파라미터가 조정되면 -- startNum/lastNum이 변경되어 출력 리스트가 변경됨 -->
+            <!-- 1. page 파라미터 변경함으로써 다음 페이지 조정  -->
+              <a class="page-link" href="./list?page=${pager.startNum-1}" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+              </a> 
+            </li> 
+            <!-- 조정된 다음 페이지를 기준으로 startNum t0 lastNum까지 반복문돌려 블럭 형성  -->
+            <c:forEach begin="${pager.startNum}" end="${pager.lastNum}" var="i">
+              <li class="page-item"><a class="page-link" href="./list?page=${i}"> ${i}</a> </li>
+            </c:forEach> 
+            <li class="page-item ${pager.next?'':'disabled'}">
+              <a class="page-link" href="./list?page=${pager.lastNum+1}" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <!-- pagination end --> 
+        
             <!-- ===========footer=============== -->
 <c:import url="../common/footer.jsp"></c:import>
 <!-- ================================ -->
